5100a16cda6572b84996a012a05c10cd448d86a5
Jacob Rein <jakerein@gmail.com>
2023-02-15 13:04:30 -0500

    Added manual ip adding and little search indicators

--- common/src/commonMain/kotlin/com/programmersbox/common/DiscoveryScreen.kt
+++ common/src/commonMain/kotlin/com/programmersbox/common/DiscoveryScreen.kt
@@ -1,11 +1,18 @@
 package com.programmersbox.common
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
@@ -15,18 +22,38 @@ import kotlinx.coroutines.CoroutineScope
 internal fun DiscoveryScreen(viewModel: PillViewModel) {
     val scope = rememberCoroutineScope()
     val vm = remember { DiscoveryViewModel(scope, viewModel) }
+    var ip by remember { mutableStateOf("") }
+
+    LaunchedEffect(Unit) { vm.startDiscovery() }
 
     Scaffold(
         bottomBar = {
-            BottomAppBar {
-                OutlinedButton(
-                    onClick = { vm.startDiscovery() },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 4.dp)
-                ) {
-                    Text("Discover")
-                }
+            Column {
+                LinearProgressIndicator(
+                    progress = animateFloatAsState(
+                        targetValue = if (vm.isSearching) 0f else 1f,
+                        animationSpec = if (vm.isSearching) tween(30000) else tween(0)
+                    ).value,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                BottomAppBar(
+                    actions = {
+                        OutlinedButton(
+                            onClick = { vm.startDiscovery() },
+                            enabled = !vm.isSearching,
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(horizontal = 4.dp)
+                        ) { Text("Discover") }
+                    },
+                    floatingActionButton = {
+                        ExtendedFloatingActionButton(
+                            text = { Text("Manual IP") },
+                            icon = { Icon(Icons.Default.Add, null) },
+                            onClick = { if (ip.isNotEmpty()) vm.connect(ip) }
+                        )
+                    }
+                )
             }
         }
     ) { padding ->
@@ -42,6 +69,38 @@ internal fun DiscoveryScreen(viewModel: PillViewModel) {
                         supportingText = { Text(it.ip) }
                     )
                 }
+            }
+
+            item {
+                AnimatedVisibility(
+                    vm.isSearching,
+                    enter = expandVertically() + fadeIn(),
+                    exit = shrinkVertically() + fadeOut(),
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CircularProgressIndicator()
+                        Text("Searching")
+                    }
+                }
+            }
+
+            item {
+                OutlinedTextField(
+                    value = ip,
+                    onValueChange = { ip = it },
+                    label = { Text("Enter ip address") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 4.dp),
+                    trailingIcon = {
+                        IconButton(
+                            onClick = { vm.connect(ip) }
+                        ) { Icon(Icons.Default.Add, null) }
+                    }
+                )
             }
         }
     }
